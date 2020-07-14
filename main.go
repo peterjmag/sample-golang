7762264635cadfe14d6acf8a09ef07fb8c9a3bd5
Peter Magenheimer <peter.magenheimer@gmail.com>
2020-07-14 12:59:35 +0200

    Add test comment

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
  	"os"
 )
 
+// Test comment
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello there! you've requested %s\n", r.URL.Path)
